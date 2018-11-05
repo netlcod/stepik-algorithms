44c4c0158bcecb3425b4f5bb02a6119272955844
netlcod <t.artem2194@gmail.com>
2018-11-05 14:50:15 +0300

    Add task4

---
+++ Week 1/tasks/task4.py
@@ -0,0 +1,45 @@
+import sys
+import re
+
+class Stack:
+    def __init__(self):
+        self.items=[]
+        self.max_items=[] 
+
+    def push(self, item):
+        if len(self.max_items)==0:
+            self.max_items.append(item)
+        elif item>self.max_items[-1]:
+            self.max_items.append(item)
+        else:
+            self.max_items.append(self.max_items[-1]) 
+        self.items.append(item)
+
+    def pop(self):
+        self.max_items.pop()
+        return self.items.pop()
+
+    def size(self):
+        return len(self.items)
+         
+    def max(self):
+        print(self.max_items[-1])
+        
+    def command(self, input_comm):
+        all_comm=('push', 'pop', 'max')
+        if all_comm[0] in input_comm:
+            val=[int(s) for s in re.findall('\d+', input_comm)][0]
+            self.push(val)
+        elif all_comm[1] in input_comm:
+            self.pop()
+        elif all_comm[2] in input_comm:
+            self.max()
+            
+def main():  
+    s=Stack()
+    num_comm=int(input())
+    for input_comm in sys.stdin:
+        s.command(input_comm)
+        
+if __name__ == "__main__":            
+    main()
